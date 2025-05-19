f469cf1d20ecb2e9c50e8fd0f21323d099f447be
amatgil <amatgilvinyes@gmail.com>
2025-05-19 21:28:15 +0200

    Fix mismatched version nonsense

--- uiuapp/src/main.rs
+++ uiuapp/src/main.rs
@@ -1,8 +1,8 @@
 #![allow(non_snake_case)]
 
+use crate::document::*;
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
-use manganis::Asset;
 
 fn main() {
     // Init logger
@@ -13,96 +13,91 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    static CSS: Asset = manganis::asset!("/assets/uiuapp.css");
-    static UIUA386: Asset = manganis::asset!("/assets/Uiua386.ttf");
+    static CSS: Asset = asset!("/assets/uiuapp.css");
+    static UIUA386: Asset = asset!("/assets/Uiua386.ttf");
 
     rsx! {
-        head {
-            meta { charset: "UTF-8" }
-            meta {
-                content: "width=device-width, initial-scale=1.0",
-                name: "viewport",
-            }
-            title { "uiuapp" }
-            link { href: "{CSS}", rel: "stylesheet" }
-            // TODO: add font here
-
+        Meta { charset: "UTF-8" }
+        Meta {
+            content: "width=device-width, initial-scale=1.0",
+            name: "viewport",
         }
-        body {
-            div { class: "wrapper",
-                div { class: "code-zone",
+        Title { "uiuapp" }
+        Stylesheet { href: CSS }
+
+        div { class: "wrapper",
+              div { class: "code-zone",
                     div { class: "code-display-zone",
-                        div { class: "code-scrollbackbuffer",
-                            p { class: "user-input", "+ 1 1" }
-                            p { class: "user-result", "2" }
-                            p { class: "user-input", "˙⊞=⇡3" }
-                            p { class: "user-result",
-                                "1 0 0"
-                                br {}
-                                "0 1 0"
-                                br {}
-                                "0 0 1"
-                            }
-                            p { class: "user-input", "˙⊞=⇡3" }
-                            p { class: "user-result",
-                                "1 0 0"
-                                br {}
-                                "0 1 0"
-                                br {}
-                                "0 0 1"
-                            }
-                            p { class: "user-input", "˙⊞=⇡3" }
-                            p { class: "user-result",
-                                "1 0 0"
-                                br {}
-                                "0 1 0"
-                                br {}
-                                "0 0 1"
-                            }
-                        }
-                        div { class: "code-buttons",
-                            button { "Settings" }
-                        }
+                          div { class: "code-scrollbackbuffer",
+                                p { class: "user-input", "+ 1 1" }
+                                p { class: "user-result", "2" }
+                                p { class: "user-input", "˙⊞=⇡3" }
+                                p { class: "user-result",
+                                    "1 0 0"
+                                    br {}
+                                    "0 1 0"
+                                    br {}
+                                    "0 0 1"
+                                }
+                                p { class: "user-input", "˙⊞=⇡3" }
+                                p { class: "user-result",
+                                    "1 0 0"
+                                    br {}
+                                    "0 1 0"
+                                    br {}
+                                    "0 0 1"
+                                }
+                                p { class: "user-input", "˙⊞=⇡3" }
+                                p { class: "user-result",
+                                    "1 0 0"
+                                    br {}
+                                    "0 1 0"
+                                    br {}
+                                    "0 0 1"
+                                }
+                          }
+                          div { class: "code-buttons",
+                                button { "Settings" }
+                          }
                     }
                     div { class: "code-textarea-zone",
-                        input { class: "uiua-input" }
-                        button { class: "run-button", "Run" }
+                          input { class: "uiua-input" }
+                          button { class: "run-button", "Run" }
                     }
-                }
-                div { class: "input-zone",
+              }
+              div { class: "input-zone",
                     div { class: "special-buttons",
-                        button { "Return" }
-                        button { ";" }
-                        button { "←" }
-                        button { "↓" }
-                        button { "↑" }
-                        button { "→" }
-                        button { "Bksp" }
+                          button { "Return" }
+                          button { ";" }
+                          button { "←" }
+                          button { "↓" }
+                          button { "↑" }
+                          button { "→" }
+                          button { "Bksp" }
                     }
                     div { class: "input-grid-buttons",
-                        button { class: "uiua-char-input dyadic-function", "+" }
-                        button { class: "uiua-char-input monadic-function", "⁅" }
-                        button { class: "uiua-char-input dyadic-function", ">" }
-                        button { class: "uiua-char-input monadic-function", "△" }
-                        button { class: "uiua-char-input", "Expr" }
-                        button { class: "uiua-char-input monadic-function", "⍉" }
-                        button { class: "uiua-char-input monadic-function", "⍆" }
-                        button { class: "uiua-char-input monadic-function", "⊚" }
-                        button { class: "uiua-char-input dyadic-modifier", "⍜" }
-                        button { class: "uiua-char-input dyadic-modifier", "⍣" }
-                        button { class: "uiua-char-input dyadic-modifier", "⊃" }
-                        button { class: "uiua-char-input dyadic-function", "⊡" }
-                        button { class: "uiua-char-input constant-value", "τ" }
-                        button { class: "uiua-char-input stack-function", "[" }
-                        button { class: "uiua-char-input string-literal", "@" }
-                        button { class: "uiua-char-input dyadic-function", "⊗" }
-                        button { class: "uiua-char-input monadic-modifier", "⊸" }
-                        button { class: "uiua-char-input stack-function", "←" }
-                        button { class: "uiua-char-input constant-value", "0" }
-                        button { class: "uiua-char-input stack-function", "∘" }
+                          button { class: "uiua-char-input dyadic-function", "+" }
+                          button { class: "uiua-char-input monadic-function", "⁅" }
+                          button { class: "uiua-char-input dyadic-function", ">" }
+                          button { class: "uiua-char-input monadic-function", "△" }
+                          button { class: "uiua-char-input", "Expr" }
+                          button { class: "uiua-char-input monadic-function", "⍉" }
+                          button { class: "uiua-char-input monadic-function", "⍆" }
+                          button { class: "uiua-char-input monadic-function", "⊚" }
+                          button { class: "uiua-char-input dyadic-modifier", "⍜" }
+                          button { class: "uiua-char-input dyadic-modifier", "⍣" }
+                          button { class: "uiua-char-input dyadic-modifier", "⊃" }
+                          button { class: "uiua-char-input dyadic-function", "⊡" }
+                          button { class: "uiua-char-input constant-value", "τ" }
+                          button { class: "uiua-char-input stack-function", "[" }
+                          button { class: "uiua-char-input string-literal", "@" }
+                          button { class: "uiua-char-input dyadic-function", "⊗" }
+                          button { class: "uiua-char-input monadic-modifier", "⊸" }
+                          button { class: "uiua-char-input stack-function", "←" }
+                          button { class: "uiua-char-input constant-value", "0" }
+                          button { class: "uiua-char-input stack-function", "∘" }
                     }
-                }
-            }
+              }
         }
     }
 }
