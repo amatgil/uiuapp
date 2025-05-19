38734eecb4623d88c10401d987de13ac0d6b4e29
amatgil <amatgilvinyes@gmail.com>
2025-05-19 20:20:41 +0200

    Include root in commits

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Cross-platform app for uiua";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs =
+    { self, nixpkgs, rust-overlay }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      overlays = [(import rust-overlay) ];
+      pkgs = (import nixpkgs { system = "x86_64-linux"; inherit overlays; });
+    in
+    {
+      #packages = forAllSystems (system: {
+      #  default = pkgs.callPackage ./default.nix { inherit pkgs; };
+      #});
+      devShells = forAllSystems (system: {
+        default = pkgs.callPackage ./shell.nix { inherit pkgs; };
+      });
+    };
+}
---
+++ shell.nix
@@ -0,0 +1,72 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib ? pkgs.lib,
+}:
+let
+  rustPlatformD = pkgs.makeRustPlatform {
+    cargo = pkgs.rust-bin.stable.latest.default.override { targets = [ "wasm32-unknown-unknown" ]; };
+    rustc = pkgs.rust-bin.stable.latest.default.override { targets = [ "wasm32-unknown-unknown" ]; };
+  };
+
+  packages = with pkgs; [
+    rust-analyzer
+    rustfmt
+    mold
+    #rust-bin.stable.latest.default
+    (rust-bin.stable.latest.default.override {
+      targets = [ "wasm32-unknown-unknown" ];
+    })
+
+    (rustPlatformD.buildRustPackage rec {
+      pname = "dioxus-cli";
+      version = "0.6.3";
+
+      src = fetchCrate {
+        inherit pname version;
+        hash =  "sha256-wuIJq+UN1q5qYW4TXivq93C9kZiPHwBW5Ty2Vpik2oY=";
+      };
+
+      cargoHash = "sha256-r4O7DPNqPVxfhbLV8mFJoEYoPW8qb+KLlsJuSASYeVQ=";
+
+      nativeBuildInputs = [
+        pkg-config
+        cacert
+      ];
+      buildInputs = [ openssl ];
+
+      OPENSSL_NO_VENDOR = 1;
+
+      nativeCheckInputs = [ rustfmt ];
+
+      passthru = {
+        updateScript = nix-update-script { };
+        tests.version = testers.testVersion { package = dioxus-cli; };
+      };
+    })
+
+
+
+
+    (rustPlatform.buildRustPackage rec {
+      pname = "wasm-bindgen-cli";
+      version = "0.2.100";
+      cargoHash = "sha256-tD0OY2PounRqsRiFh8Js5nyknQ809ZcHMvCOLrvYHRE=";
+
+      src = fetchCrate {
+        inherit pname version;
+        hash = "sha256-3RJzK7mkYFrs7C/WkhW9Rr4LdP5ofb2FdYGz1P7Uxog=";
+      };
+
+      buildInputs = [ openssl ];
+      nativeBuildInputs = [ pkg-config ];
+    })
+  ];
+in
+pkgs.mkShell {
+  nativeBuildInputs = packages;
+  buildInputs = packages;
+  env = {
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}";
+  };
+}
